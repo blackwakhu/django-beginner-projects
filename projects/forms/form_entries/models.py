8157ffa629147ea9fe0c2b9e376d1f5c7b8cfc45
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-24 15:36:45 +0300

    added the author and book into the models

--- projects/forms/form_entries/models.py
+++ projects/forms/form_entries/models.py
@@ -1,1 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+  fname =  models.CharField(max_length=25)
+  lname = models.CharField(max_length=25)
+  email = models.EmailField(max_length=50)
+  
+  def __str__(self):
+    return f"{fname} {lname}"
+
+class Book(models.Model):
+  isdn = models.CharField(max_length=10) 
+  title = models.CharField(max_length=25)
+  year = models.IntegerField(default=2000) 
+  genre = models.CharField(max_length=25)
+  edition = models.IntegerField(default=1)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
 
