1d09e890fe66d2d63f2621ea90c43f7ea8521d3a
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-24 16:34:44 +0300

    added books to views

--- projects/forms/form_entries/views.py
+++ projects/forms/form_entries/views.py
@@ -3,3 +3,6 @@ from .models import *
 
 def allAuthor(request):
   return render(request, "allAuthor.html", {"authors": Author.objects.all()})
+
+def allBook(request):
+  return render(request, "allBook.html", {"books": Book.objects.all()})
