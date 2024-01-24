190ba3317a5375704284e3a5198bfb4367b612ca
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-24 16:21:59 +0300

    added author to the views

--- projects/forms/form_entries/views.py
+++ projects/forms/form_entries/views.py
@@ -1,1 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
+def allAuthor(request):
+  return render(request, "allAuthor.html", {"authors": Author.objects.all()})
