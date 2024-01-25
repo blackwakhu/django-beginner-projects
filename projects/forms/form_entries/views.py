1981baaf93c4a2ebde70b9427a82cb32563c9dd2
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-25 10:26:26 +0300

    added a create author view

--- projects/forms/form_entries/views.py
+++ projects/forms/form_entries/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .models import Author, Book
+from .forms import AuthorForm
 
 # Create your views here.
 
@@ -15,9 +16,17 @@ def allBook(request):
   return render(request, "allBook.html", {"books": Book.objects.all()})
 
 def author(request, author_id):
-  return render(request, "author.html", {"data": get_object_or_404(Author, pk=author_id)})
-
+  return render(request, "author.html", {"data": get_object_or_404(Author, pk=author_id), "books": Book.objects.filter(author_id=author_id)})
 
 def book(request, book_id):
   return render(request, "book.html", {"data": get_object_or_404(Book, pk=book_id)})
+
+def createAuthor(request):
+  form = AuthorForm()
+  if request.method == "POST":
+    form = AuthorForm(request.POST)
+    if form.is_valid():
+      form.save()
+    return redirect("/form/authors")
+  return render(request, "createAuthor.html", {"form": form})
 
