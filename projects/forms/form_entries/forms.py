e09ae14bcae9f20112a22798984f16fc97a0478c
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-25 11:08:38 +0300

    added the book form

--- projects/forms/form_entries/forms.py
+++ projects/forms/form_entries/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import * 
+from .models import *
 
 
 class AuthorForm(forms.ModelForm):
-  class Meta:
-    model = Author
-    fields = "__all__"
+    class Meta:
+        model = Author
+        fields = "__all__"
+
+
+class BookForm(forms.Form):
+    isdn = forms.CharField(max_length=10)
+    title = forms.CharField(max_length=25)
+    year = forms.IntegerField()
+    genre = forms.CharField(max_length=25)
+    edition = forms.IntegerField()
