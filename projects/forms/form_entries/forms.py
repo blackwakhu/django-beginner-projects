41158221ef12ad12a40296af040b7a9c5e4e6737
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-26 14:28:13 +0300

    increased size of book title

--- projects/forms/form_entries/forms.py
+++ projects/forms/form_entries/forms.py
@@ -10,7 +10,7 @@ class AuthorForm(forms.ModelForm):
 
 class BookForm(forms.Form):
     isdn = forms.CharField(max_length=10)
-    title = forms.CharField(max_length=25)
+    title = forms.CharField(max_length=100)
     year = forms.IntegerField()
     genre = forms.CharField(max_length=25)
     edition = forms.IntegerField()
