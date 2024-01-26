59fad20ea979d184846533e256139d2b1749d985
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-26 14:25:48 +0300

    increased size on book title

--- projects/forms/form_entries/models.py
+++ projects/forms/form_entries/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. password => westlife email => west@gmail.com uname=> runner
 
 class Author(models.Model):
   fname =  models.CharField(max_length=25)
   lname = models.CharField(max_length=25)
   email = models.EmailField(max_length=50)
-  
+
   def __str__(self):
     return f"{self.fname} {self.lname}"
 
 class Book(models.Model):
   isdn = models.CharField(max_length=10) 
-  title = models.CharField(max_length=25)
+  title = models.CharField(max_length=100)
   year = models.IntegerField(default=2000) 
   genre = models.CharField(max_length=25)
   edition = models.IntegerField(default=1)
@@ -20,4 +20,3 @@ class Book(models.Model):
 
   def __str__(self):
     return self.title
-
