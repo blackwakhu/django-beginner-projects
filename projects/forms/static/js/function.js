5e814c205578905d023907f4986e9cd9bfbf51e6
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-26 16:23:39 +0300

    added the visibility code

--- projects/forms/static/js/function.js
+++ projects/forms/static/js/function.js
@@ -1,4 +1,17 @@
-
 function deletedata(){
   window.alert("you are deleting the piece of data");
+}
+
+function open_data(btn, input){
+  let mybtn = document.getElementById(btn)
+  let myinput = document.getElementById(input)
+  myinput.style.display = "block"
+  mybtn.style.display = "none"
+}
+
+function close_data(btn, input){
+  let mybtn = document.getElementById(btn)
+  let myinput = document.getElementById(input)
+  myinput.style.display = "none"
+  mybtn.style.display = "block"
 }
